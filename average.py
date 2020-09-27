999824701d2800c84be2b63ef847a2ba51613235
Mark Hetherington <mark@keodesign.com.au>
2020-09-28 08:29:17 +1000

    Plot average for now

--- average.py
+++ average.py
@@ -1,18 +1,29 @@
-import os
-import json
+import matplotlib.pyplot as plt
 
-from constants import *
 # Produces an "average" day from all the data.
 from utils import load_pandas, load_all_data
-import pandas as pd
 
 
 data = load_all_data(limit=60)
 data = data.resample('T').mean()
 
 by_time = data.groupby(data.index.time).mean()
+print("total samples:%d, grouped samples %d" % (len(data.index),len(by_time.index)))
 
-if not os.path.exists(output_directory):
-    os.mkdir(output_directory)
+#df.index = df.index.time
+by_time['ACLoad.P'].plot(label="ACLOAD", zorder=1)
+by_time['PV.P'].plot(label="PV", zorder=1)
+#by_time['Battery.P'].plot(label="Battery", zorder=1)
 
-by_time.to_json(os.path.join(output_directory, "average.json"))
+# multiple line plot
+# plt.plot('Date', 'ACLoad.P', data=df)
+# plt.plot('Date', 'PV.P', data=df)
+# plt.plot('Date', 'Grid.P', data=df)
+# plt.plot('Date', 'Battery.P', data=df)
+plt.legend()
+plt.show()
+
+# if not os.path.exists(output_directory):
+#     os.mkdir(output_directory)
+#
+# by_time.to_json(os.path.join(output_directory, "average.json"))
