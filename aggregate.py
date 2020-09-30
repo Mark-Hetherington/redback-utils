b2714d3ac1ba3502903577560c7e36032f865623
Mark Hetherington <mark@keodesign.com.au>
2020-10-01 07:22:49 +1000

    add progress messages

--- aggregate.py
+++ aggregate.py
@@ -4,7 +4,11 @@ from constants import data_directory
 from utils import load_all_json_data, convert_columns_types, resample_dataframe
 
 data = load_all_json_data()
+print("Converting column types")
 convert_columns_types(data)
+print("Writing complete data set")
 data.to_hdf(os.path.join(data_directory, "all.h5"), 'table')
+print("Resampling to by minute samples")
 data = resample_dataframe(data, 'T')
+print("Writing by minute samples")
 data.to_hdf(os.path.join(data_directory, "byminute.h5"), 'table')
