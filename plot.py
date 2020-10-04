410c7b0306e4fa3d9533f665827757789867720e
Mark Hetherington <mark@keodesign.com.au>
2020-10-04 19:52:11 +1100

    Allow plotting a specific day from a given file

--- plot.py
+++ plot.py
@@ -1,8 +1,9 @@
 # libraries
 import matplotlib.pyplot as plt
+import sys, getopt
 
 from constants import data_directory
-from utils import load_file, resample_dataframe, simulation_stats
+from utils import load_file, resample_dataframe, dataset_stats, load_all_data
 import numpy as np
 import pandas as pd
 from datetime import date, timedelta
@@ -13,9 +14,40 @@ import os
 #                    'y3': np.random.randn(10) + range(11, 21)})
 
 #df = load_file('output/average.json')
-df = load_file('output/simulation-mean-day-Full upgrade.h5')
+
+command_text = """
+{0} -f <file> [-d <date>]
+e.g. {0} -f 'output/simulation-Full upgrade.h5' -f '2020-01-01'
+    -f, --file      Specify file
+    -d, --date      Specify date
+"""
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "f:d:", ["file=", "date="])
+except getopt.GetoptError as err:
+    print(err)
+    print(command_text)
+    sys.exit(2)
+
+datafile = None
+date = None
+for opt, arg in opts:
+    if opt == '-h':
+        print(command_text)
+        sys.exit()
+    elif opt in ("-f", "--file"):
+        datafile = arg
+    elif opt in ("-d", "--date"):
+        date = arg
+
+df = load_file(datafile) if datafile else load_all_data()
+if hasattr(df.index, 'floor') and date:
+    df = df[df.index.floor('D') == date]
+
+dataset_stats(df)
+
 if 'simulation.Grid.P' in df:
-    simulation_stats(df)
     df["ACLoad.P"].plot(label="Load")
     df["simulation.PV.P"].plot(label="PV")
     # data["PV.P"].plot()
